135853c4913e13bf375eb4e3eb60118698083cab
PotatoJoe111 <dbu99r@protonmail.com>
2024-11-06 06:19:01 +0100

    Command parsing: removing whitespaces

--- command_parser.c
+++ command_parser.c
@@ -1,3 +1,32 @@
-void execute(int command){
-	
+#include <stdio.h>
+
+void parse(char *command){
+
+	//calculating command length
+	int command_length = 0;
+	for(int i=0;command[i]!=0;i++){
+		command_length++;
+	}
+	command_length--;
+
+	//remove trailing and whitespaces
+	char clean_command[command_length];
+	int index = 0;
+	for(int i=0;i<command_length;i++){
+		if(i==0 && command[i]==' '){
+
+		}
+		else if(i==0 && command[i]!=' '){
+			clean_command[index]=command[i];
+			index++;
+		}
+		else if(command[i]!=' ' || command[i]==' ' && command[i-1]!=' '){
+			clean_command[index]=command[i];
+			index++;
+		}
+	}
+}
+
+void execute(char *command){
+	parse(command);
 }
--- command_parser.h
+++ command_parser.h
@@ -1,1 +1,2 @@
+void parse(char*);
 void execute(char*);
