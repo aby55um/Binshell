d2de46e7bda02c3cf7f9bfcbef1b306751d2d009
PotatoJoe111 <dbu99r@protonmail.com>
2024-11-07 05:52:34 +0100

    added the status variable

--- command_execute.c
+++ command_execute.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "variables.h"
 
 void execute_command(char** token_list){
 	
--- input_loop.c
+++ input_loop.c
@@ -3,11 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "command_parser.h"
+#include "variables.h"
 
 #define CMD_SIZE 100
 
 void input_loop(){
-	while(1){
+	while(state == 1){
 
 		int buf_size = CMD_SIZE * sizeof(char);
 		char *command = malloc(buf_size);
