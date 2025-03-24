4184f5200687c48385119c4eb288189449113896
dbu99r@protonmail.com <dbu99r@protonmail.com>
2025-03-24 07:08:39 +0100

    Added basic segment information display

--- command_execute.c
+++ command_execute.c
@@ -59,7 +59,7 @@ void execute_command(char** token_list){
 				fseek(fl, 0, SEEK_END);
 				long file_size = ftell(fl);
 				fseek(fl, 0, SEEK_SET);
-				char *buffer = (char *)malloc(file_size + 1);
+				unsigned char *buffer = (unsigned char *)malloc(file_size + 1);
 				fread(buffer, 1, file_size, fl);
 				fclose(fl);
 
@@ -85,10 +85,17 @@ void execute_command(char** token_list){
 					little_endian_read(buffer, 27 + 4 * b64, 4 + 4 * b64, 0);
 					printf("Program header table offset: 0x");
 					little_endian_read(buffer, 31 + 8 * b64, 4 + 4 * b64, 0);
+					int program_header_table_offset = 0;
+					for(int i=0;i < 4 + 4 * b64;i++){
+						program_header_table_offset += buffer[28 + 4 * b64 + i] * pow(16,(double)i);
+						//printf("%d\n",buffer[28 + 4 * b64 +i]);
+					}
+					printf("%d",program_header_table_offset);
 					printf("Section header table offset: 0x");
 					little_endian_read(buffer, 35 + 12 * b64, 4 + 4 * b64, 0);
 					printf("Number of program header entries: ");
 					little_endian_read(buffer, 45 + 12 * b64, 2, 1);
+					int prog_header_entry_number = buffer[44 + 12 * b64] + 16 * buffer[45 + 12 * b64];
 					printf("Program header table entry size: ");
 					int prog_header_table_entry_size = 16 * buffer[43 + 12 * b64] + buffer[42 + 12 * b64];
 					printf("%d\n", prog_header_table_entry_size);
@@ -99,6 +106,16 @@ void execute_command(char** token_list){
 					printf("%d\n", section_header_table_entry_size);
 					printf("Section index to the section header string table: 0x");
 					little_endian_read(buffer, 51 + 12 * b64, 2, 0);
+
+					/*printf("First program header: ");
+					for(int i=0;i<prog_header_table_entry_size;i++){
+						printf("%d ",buffer[program_header_table_offset + i]);
+					}*/
+
+					printf("Segment types:\n");
+					for(int i=0;i<prog_header_entry_number;i++){
+						little_endian_read(buffer, program_header_table_offset + i * prog_header_table_entry_size + 2, 3, 0);
+					}
 				}
 
 				// Old code, to be deleted
